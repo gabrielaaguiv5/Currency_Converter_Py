f4c4c15add4a076e19c8e6ffc3f0de696b40ee2d
Gabriela Aguilar <gabrielaaguiv@gmail.com>
2026-01-19 15:25:46 -0500

    Refactor font configuration for labels and buttons in Currency Converter GUI

--- currencyconverter.py
+++ currencyconverter.py
@@ -8,9 +8,9 @@ class CurrencyConverter:
         window.configure(bg="magenta")
         window.geometry("400x200")
         # Labels and Entries
-        Label(window, font = "Albert Sans 12 bold", bg = "magenta", text = "Amount to Convert:").grid(row=1, column=1, sticky=W)
-        Label(window, font = "Albert Sans 12 bold", bg = "magenta", text = "Conversion Rate:").grid(row=2, column=1, sticky=W)
-        Label(window, font = "Albert Sans 12 bold", bg = "magenta", text = "Converted Amount:").grid(row=3, column=1, sticky=W)
+        Label(window, font = ("Albert Sans", 12, "bold"), bg = "magenta", text = "Amount to Convert:").grid(row=1, column=1, sticky=W)
+        Label(window, font = ("Albert Sans", 12, "bold"), bg = "magenta", text = "Conversion Rate:").grid(row=2, column=1, sticky=W)
+        Label(window, font = ("Albert Sans", 12, "bold"), bg = "magenta", text = "Converted Amount:").grid(row=3, column=1, sticky=W)
         # Object Creation
         self.amount_to_convert = StringVar() # StringVar Object is a Class from tkinter
         # Entry Configuration
@@ -19,12 +19,12 @@ class CurrencyConverter:
         Entry(window, textvariable = self.conversion_rate, justify = RIGHT).grid(row=2, column=2)
         self.converted_amount = StringVar()
         # For Converted Amount no user input is needed
-        lblConverted_amount = Label(window, font = "Albert Sans 12 bold", bg = "magenta", textvariable = self.converted_amount).grid(row=3, column=2, sticky=E)
+        lblConverted_amount = Label(window, font = ("Albert Sans", 12, "bold"), bg = "magenta", textvariable = self.converted_amount).grid(row=3, column=2, sticky=E)
         # Button Configuration
         # Button to perform conversion
-        btConvertedAmount = Button(window, font = "Albert Sans 12 bold", bg = "White", fg= "Black", command = self.converted_amount).grid(row=4, column=2, sticky=E) #self.converted_amount is a function that will be created later
+        btConvertedAmount = Button(window, font = ("Albert Sans", 12, "bold"), bg = "White", fg= "Black", command = self.converted_amount).grid(row=4, column=2, sticky=E) #self.converted_amount is a function that will be created later
         # Button to clear all fields
-        btClear = Button(window, font = "Albert Sans 12 bold", bg = "White", fg = "Black", command = self.delete_all).grid(row=4, column=6, padx=25, pady=25,sticky=E)
+        btClear = Button(window, font = ("Albert Sans", 12, "bold"), bg = "White", fg = "Black", command = self.delete_all).grid(row=4, column=6, padx=25, pady=25,sticky=E)
     
         window.mainloop() # Start the GUI event loop
 
